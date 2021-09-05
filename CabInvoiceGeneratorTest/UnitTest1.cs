55301ce8a5596853b649a9899bc14424acb58c82
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-05 20:49:50 +0530

    CabInvoiceGenerator

---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabInvoiceGeneratorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        [TestMethod]
+        public void GivenDistanceTime
+        {
+        }
+    }
+    public void Give
+}
